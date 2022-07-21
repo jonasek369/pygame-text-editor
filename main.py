aae20ad9de54bf0e9687ca310df90fee5edd2401
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2022-07-21 09:46:02 +0200

    added the Vim like command line and few commands with it

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ default_settings = {
     "window-resolution": [1280, 720],
     "window-refreshrate": 240,
     "color-background": [24, 24, 24],
-    "color-text": [255, 255, 255]
+    "color-text": [255, 255, 255],
+    "color-commandline": [0, 0, 255]
 }
 
 # load aliases for easier and more readable use
@@ -62,7 +63,7 @@ class Cursor:
 
 class Buffer:
     def __init__(self):
-        self.data = ["import math", "def main():", "    return math.sqrt(1)"]
+        self.data = [""]
 
 
 class Editor:
@@ -76,24 +77,44 @@ class Editor:
             self.screen = pygame.display.set_mode(get_setting("window-resolution"))
 
         self.running = True
+        self.cmd_mode = False
 
         self.window = TextWindow(self.screen)
         self.buffer = Buffer()
+        self.cmd_buffer = Buffer()
         self.cursor = Cursor()
+        self.cmd_cursor = Cursor()
+
+        self.operating_file_name = "untitled"
 
     def start(self):
         self.loop()
 
     def draw_buffer(self):
         size = get_setting("font-size")
+
+        # draw vim like command line
+        if self.cmd_mode:
+            res = get_setting("window-resolution")
+            pygame.draw.rect(self.screen, get_setting("color-commandline"),
+                             pygame.Rect([0, res[1] - size], [res[0], size]))
+
+            for pos, line in enumerate(self.cmd_buffer.data):
+                render = self.text_renderer.render(line, get_setting("font-antialias"), get_setting("color-text"))
+                self.screen.blit(render, (0, res[1] - size))
+
         # draw cursor
-        text = self.buffer.data[self.cursor.pos.y][:self.cursor.pos.x]
+        current_buffer = self.cmd_buffer.data if self.cmd_mode else self.buffer.data
+        current_cursor = self.cmd_cursor if self.cmd_mode else self.cursor
+        start_pos = get_setting("window-resolution")[1] - size if self.cmd_mode else self.cursor.pos.y * size
+
+        text = current_buffer[current_cursor.pos.y][:current_cursor.pos.x]
         pre_render = self.text_renderer.render(text, get_setting("font-antialias"), get_setting("color-text"))
         pygame.draw.line(self.screen, get_setting("color-text"),
-                         [pre_render.get_width(), self.cursor.pos.y * size],
-                         [pre_render.get_width(), (self.cursor.pos.y * size) + pre_render.get_height()]
+                         [pre_render.get_width(), start_pos],
+                         [pre_render.get_width(), start_pos + pre_render.get_height()], 2
                          )
-        self.cursor.pos.x = len(text)
+        current_cursor.pos.x = len(text)
 
         # draw buffer
         for pos, line in enumerate(self.buffer.data):
@@ -101,47 +122,87 @@ class Editor:
             self.screen.blit(render, (0, pos * size))
 
     def handle_texinput_event(self, event: pygame.event.Event):
-        self.buffer.data[self.cursor.pos.y] = self.buffer.data[self.cursor.pos.y][:self.cursor.pos.x] + event.text + \
-                                              self.buffer.data[self.cursor.pos.y][self.cursor.pos.x:]
-        self.cursor.pos.x += 1
+        current_buffer = self.cmd_buffer.data if self.cmd_mode else self.buffer.data
+        current_cursor = self.cmd_cursor if self.cmd_mode else self.cursor
+
+        current_buffer[current_cursor.pos.y] = current_buffer[current_cursor.pos.y][
+                                               :current_cursor.pos.x] + event.text + \
+                                               current_buffer[current_cursor.pos.y][current_cursor.pos.x:]
+        current_cursor.pos.x += 1
+
+    def parse_command(self):
+        buf = self.cmd_buffer.data
+        cmd = buf[0].lower().split(" ")
+        if cmd[0] == ":w":
+            with open(self.operating_file_name, "w") as file:
+                text = "\n".join(self.buffer.data)
+                file.write(text)
+        if cmd[0] == ":chn":
+            self.operating_file_name = "".join(cmd[1:])
+        if cmd[0] == ":sav":
+            name = "".join(cmd[1:])
+            with open(name, "w") as file:
+                text = "\n".join(self.buffer.data)
+                file.write(text)
+        if cmd[0] == ":q":
+            exit(0)
 
     def handle_key_event(self, event: pygame.event.Event):
-        if event.key == pygame.K_UP:
-            if not self.cursor.pos.y <= 0:
-                self.cursor.pos.y -= 1
-        if event.key == pygame.K_DOWN:
-            self.cursor.pos.y += 1
+        current_buffer = self.cmd_buffer.data if self.cmd_mode else self.buffer.data
+        current_cursor = self.cmd_cursor if self.cmd_mode else self.cursor
 
-        # add newline to the buffer
+        if not self.cmd_mode:
+            if event.key == pygame.K_UP:
+                if not self.cursor.pos.y <= 0:
+                    self.cursor.pos.y -= 1
+            if event.key == pygame.K_DOWN:
+                self.cursor.pos.y += 1
+
         for i in range(self.cursor.pos.y - len(self.buffer.data) + 1):
             self.buffer.data.append("")
 
         if event.key == pygame.K_LEFT:
-            if not self.cursor.pos.x <= 0:
-                self.cursor.pos.x -= 1
+            if not current_cursor.pos.x <= 0:
+                current_cursor.pos.x -= 1
         if event.key == pygame.K_RIGHT:
-            if len(self.buffer.data[self.cursor.pos.y]) >= self.cursor.pos.x + 1:
-                self.cursor.pos.x += 1
+            if len(current_buffer[current_cursor.pos.y]) >= current_cursor.pos.x + 1:
+                current_cursor.pos.x += 1
 
         if event.key == pygame.K_BACKSPACE:
-            if self.cursor.pos.x == 0 and self.cursor.pos.y != 0:
-                removed = self.buffer.data.pop(self.cursor.pos.y)
-                self.cursor.pos.y -= 1
-                self.cursor.pos.x = len(self.buffer.data[self.cursor.pos.y])
-                self.buffer.data[self.cursor.pos.y] += removed
+            if current_cursor.pos.x == 0 and current_cursor.pos.y != 0:
+                removed = current_buffer.pop(current_cursor.pos.y)
+                current_cursor.pos.y -= 1
+                current_cursor.pos.x = len(current_buffer[current_cursor.pos.y])
+                current_buffer[current_cursor.pos.y] += removed
             else:
-                if not self.cursor.pos.x <= 0:
-                    self.buffer.data[self.cursor.pos.y] = self.buffer.data[self.cursor.pos.y][:self.cursor.pos.x - 1] + \
-                                                          self.buffer.data[self.cursor.pos.y][self.cursor.pos.x:]
-                    self.cursor.pos.x -= 1
+                if not current_cursor.pos.x <= 0:
+                    current_buffer[current_cursor.pos.y] = current_buffer[current_cursor.pos.y][
+                                                           :current_cursor.pos.x - 1] + \
+                                                           current_buffer[current_cursor.pos.y][current_cursor.pos.x:]
+                    current_cursor.pos.x -= 1
+
+        if event.key == pygame.K_ESCAPE:
+            self.cmd_mode = not self.cmd_mode
+            if not self.cmd_mode:
+                self.cmd_buffer.data = [""]
 
         if event.key == pygame.K_RETURN:
-            first, second = self.buffer.data[self.cursor.pos.y][:self.cursor.pos.x], self.buffer.data[self.cursor.pos.y][self.cursor.pos.x:]
-            self.buffer.data.pop(self.cursor.pos.y)
-            self.buffer.data.insert(self.cursor.pos.y, first)
-            self.buffer.data.insert(self.cursor.pos.y+1, second)
+            if self.cmd_mode:
+                self.parse_command()
+                self.cmd_mode = False
+                self.cmd_buffer.data = [""]
+            else:
+                first, second = self.buffer.data[self.cursor.pos.y][:self.cursor.pos.x], self.buffer.data[
+                                                                                             self.cursor.pos.y][
+                                                                                         self.cursor.pos.x:]
+                self.buffer.data.pop(self.cursor.pos.y)
+                self.buffer.data.insert(self.cursor.pos.y, first)
+                self.buffer.data.insert(self.cursor.pos.y + 1, second)
+                self.cursor.pos.y += 1
+                self.cursor.pos.x = len(self.buffer.data[self.cursor.pos.y]) - len(second)
 
     def loop(self):
+        dt = 0
         while self.running:
             self.screen.fill(BACKGROUND)
             for event in pygame.event.get():
@@ -153,7 +214,8 @@ class Editor:
                     self.handle_key_event(event)
             self.draw_buffer()
             pygame.display.flip()
-            self.clock.tick(FPS)
+            pygame.display.set_caption(self.operating_file_name)
+            dt = self.clock.tick(FPS)
 
 
 if __name__ == '__main__':
